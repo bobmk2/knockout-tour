ffa5c27d5b18b947915539ebfdf8a4527a726735
bobmk2 <booby.bob.mk2@gmail.com>
2018-10-27 13:29:56 +0900

    install material ui

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        Hello World!! 🍣
+        Hello World!!! 🍣
+        <MuiThemeProvider>
+          <RaisedButton label="test" />
+        </MuiThemeProvider>
       </div>
     )
   }
--- src/server/index.js
+++ src/server/index.js
@@ -4,12 +4,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/../../public'));
+app.use(express.static(__dirname + '/../../dist'));
 
 app.get('/hello-world', (req, res) => {
   res.json({hello: 'world'});
 });
 
-app.get('/js/client.js', (req, res) => {
+app.get('/scripts/client.js', (req, res) => {
   res.sendFile('client.js', {root: __dirname + '/../../dist'});
 });
 
